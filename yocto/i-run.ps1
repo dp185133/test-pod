a7cd72fbe18f30270ba585092d28f75039e9a672
MattPlacek <aphinmi@gmail.com>
2023-06-14 12:57:56 -0400

    tweaking publish of ports, bddservices startup and bashrc

--- yocto/i-run.ps1
+++ yocto/i-run.ps1
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+function publishports () {
+    echo "-p 8182:8182 -p 3131:3131 -p 5006:5006/udp -p 1127:1127 -p 1128:1128/udp -p 5150:5150"
+}
 echo --% >/dev/null;: ' | out-null
 <#'
+JAGIP="192.168.6.2"
 
 if [ "$1" == "" ]; then
     IMAGE="jacto:latest"
@@ -26,7 +30,7 @@ if false; then
     docker run -h panther2 --name jag3 --net=host --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix" --volume="/home/matt/.Xauthority:/opt/.Xauthority" --volume="/home/matt/persist:/persist" -d -i -t $IMAGE  /bin/bash
 else
     echo running no X no host network
-    docker run -h panther2 --name jag3 --volume="/home/matt/persist:/persist" -d -i -t $IMAGE  /bin/bash
+    docker run -h panther2 --name jag3 --volume="/home/matt/persist:/persist" `publishports` -d -i -t $IMAGE  /bin/bash
 fi
 
 docker exec -it jag3 /opt/config/docker-configure-run.sh
@@ -38,7 +42,7 @@ $IMAGE="jacto:latest"
 if ($false) {
     docker run -h panther2 --name jag3 --net=host --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix" --volume="/home/matt/.Xauthority:/opt/.Xauthority" --volume="/home/matt/persist:/persist" -d -i -t $IMAGE  /bin/bash
 } else {
-    docker run -h panther2 --name jag3 --volume="/persist:/persist" -d -i -t $IMAGE  /bin/bash
+    docker run -h panther2 --name jag3 --volume="/persist:/persist" $(publishports) -d -i -t $IMAGE  /bin/bash
 }
 
 docker exec -it jag3 /opt/config/docker-configure-run.sh
