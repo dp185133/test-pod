0bc732d9ed4544ee23d5237fbced426072e4d64e
MattPlacek <aphinmi@gmail.com>
2024-03-13 13:11:57 -0400

    updating to 0.1.10; latest config; etc

--- yocto/i-build.ps1
+++ yocto/i-build.ps1
@@ -5,6 +5,7 @@ echo --% >/dev/null;: ' | out-null
 VXFUEL_VER=$(ls -1rt EmeraldForecourtServices_*.cab | tail -n1 | awk 'match($0, /([0-9].*[0-9])/, ver) { print ver[1] }')
 VXFUEL_CAB=EmeraldForecourtServices_${VXFUEL_VER}.cab
 VXFUEL_MQTT=JAG_MQTT_Binaries_${VXFUEL_VER}.zip
+VXFUEL_IMAGE_NAME=vxfuel
 
 ########################################################################################
 # https://stackoverflow.com/questions/39421131/is-it-possible-to-write-one-script-that-runs-in-bash-shell-and-powershell
@@ -14,5 +15,5 @@ rm config.tgz; tar -C ~/git/panther/panther-linux-install-bundler/build/panther2
 
 #>
 
-echo docker build -t localhost:5001/vxfuel --build-arg "VXFUEL_CAB=$VXFUEL_CAB" --build-arg "VXFUEL_MQTT=$VXFUEL_MQTT" -f Docker-yoctoplus .
-docker build -t localhost:5001/vxfuel --build-arg "VXFUEL_CAB=$VXFUEL_CAB" --build-arg "VXFUEL_MQTT=$VXFUEL_MQTT" -f Docker-yoctoplus .
+echo docker build -t $VXFUEL_IMAGE_NAME --build-arg "VXFUEL_CAB=$VXFUEL_CAB" --build-arg "VXFUEL_MQTT=$VXFUEL_MQTT" -f Docker-yoctoplus .
+docker build -t $VXFUEL_IMAGE_NAME --build-arg "VXFUEL_CAB=$VXFUEL_CAB" --build-arg "VXFUEL_MQTT=$VXFUEL_MQTT" -f Docker-yoctoplus .
